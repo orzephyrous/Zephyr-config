fd2187fb48763545fd1d8284414f5ab12e82bd67
Zephyr <1416385800@qq.com>
2023-01-08 10:23:53 +0800

    lf: jump with Chinese or esc-codes in filename.

--- lf/lf-jump.cmd
+++ lf/lf-jump.cmd
@@ -1,5 +1,6 @@
 @echo off
+chcp 65001>nul
 for /f %%a in ('fzf --reverse --header="Jump to location"') do (
   lf -remote "send %1 cd \"%%~dpa\""
-  lf -remote "send %1 select \"%%a\""
+  lf -remote "send %1 select '%%a'"
 )
