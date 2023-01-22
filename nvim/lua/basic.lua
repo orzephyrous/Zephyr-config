b67cbbd24bd4d7180fb0727e86f6d4725c339ea9
Zephyr <1416385800@qq.com>
2023-01-22 10:54:44 +0800

    nvim: minor modification, config for nvim-qt.

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -3,7 +3,7 @@ require('impatient')
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.list = true
-vim.opt.listchars:append("tab:>-,space:·,lead:·,trail:·,nbsp:⍽")
+vim.opt.listchars:append("tab:-->,space:·,lead:·,trail:·,nbsp:⍽")
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
