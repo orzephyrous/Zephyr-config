997ae45307f13c625926630ca0de3faf227d1ac4
Zephyr <1416385800@qq.com>
2023-07-26 14:58:42 +0800

    nvim: treesitter json

--- nvim/lua/plugins/configs/nvim-treesitter.lua
+++ nvim/lua/plugins/configs/nvim-treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "python", "markdown", "markdown_inline", "toml", "yaml", "julia", "r", "hjson" },
+  ensure_installed = { "c", "lua", "vim", "python", "markdown", "markdown_inline", "toml", "yaml", "julia", "r", "hjson", "json" },
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
