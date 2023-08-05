2a342f13d34a8b5bdb2ef75fee2e9918f8daf11a
Zephyr <1416385800@qq.com>
2023-08-05 19:22:40 +0800

    nvim: add signcolumn for gitsigns, set fold fill

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -1,8 +1,10 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 5
 vim.opt.list = true
-vim.opt.listchars:append("tab:-->,space:·,lead:·,trail:·,nbsp:⍽")
+vim.opt.listchars:append("tab:-->,space:·,lead:·,trail:·,nbsp:⎵")
+vim.opt.fillchars:append("fold: ")
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
