4c8dfc135bd6a9ad8a148379c12e5237e887265f
Zephyr <1416385800@qq.com>
2023-09-08 22:20:56 +0800

    nvim: highlight yank

--- nvim/lua/core.lua
+++ nvim/lua/core.lua
@@ -16,3 +16,10 @@ vim.opt.termguicolors = true
 vim.opt.timeoutlen = 500
 
 vim.g.mapleader = " "
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
